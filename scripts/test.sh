5ecc9dcc20e3cfd1dea86026da50212b07c5fdbb
PlasticHub <baumgart.guenter02@gmail.com>
2020-04-16 23:57:47 +0200

    clean

--- scripts/test.sh
+++
@@ -1,3 +0,0 @@
- curl "https://books.zoho.com/api/v3/bills/2173235000000219222/attachment?organization_id=10234695" \
--H "Authorization: Zoho-oauthtoken 1000.41d9f2cfbd1b7a8f9e314b7aff7bc2d1.8fcc9810810a216793f385b9dd6e125f" \
--H "Content-Type: application/x-www-form-urlencoded;charset=UTF-8" 
