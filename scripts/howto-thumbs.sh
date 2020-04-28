123a80397ee880a875c8ab837bdc1c2b34ab285c
cat23x <baumgart.guenter02@gmail.com>
2020-04-28 17:59:13 +0200

    npm cli : jekyll server build

--- scripts/howto-thumbs.sh
+++ scripts/howto-thumbs.sh
@@ -3,28 +3,28 @@
 # Base directory for this entire project
 BASEDIR=$(cd $(dirname $0) && pwd)
 
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/brackets"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/controlbox"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/controlbox/dinrail_layout"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/controlbox/wiring"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/frame"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/hopper"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/flange"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/screw_alignment"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/keyway"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/screw_mount"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/shaft_clamp"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/clamp_flange"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/nozzle_interface"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/barrel_mount"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/hopper_mount"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/thermo_couple_clips"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/sheetpress/cartridge_heater/hole/block_vise"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/sheetpress/cartridge_heater/hole/drilling"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/sheetpress/cartridge_heater/hole/lathe_drill_addon"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/couplings/clamp"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/couplings/keyway"
-ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/resin-cast-barrel-injector"
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/brackets" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/controlbox" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/controlbox/dinrail_layout" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/controlbox/wiring" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/frame" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/hopper" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/flange" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/screw_alignment" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/keyway" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/screw_mount" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/shaft_clamp" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/clamp_flange" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/nozzle_interface" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/barrel_mount" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/hopper_mount" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/extrusion/thermo_couple_clips" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/sheetpress/cartridge_heater/hole/block_vise" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/sheetpress/cartridge_heater/hole/drilling" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/sheetpress/cartridge_heater/hole/lathe_drill_addon" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/couplings/clamp" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/couplings/keyway" --outfile=index.md
+ph-cli md:thumbs --root="../"  --debug=true --source="../_howto/resin-cast-barrel-injector" --outfile=index.md
 
 cd ../_howto
 git add -A .
