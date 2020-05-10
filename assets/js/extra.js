879d37967b0391f8ebdebff0331b1a24d02fc81a
cat23x <baumgart.guenter02@gmail.com>
2020-05-10 13:46:44 +0200

    jekyll:machine:bootstrap hash fix

---
+++ assets/js/extra.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function () {
+
+    var url = document.location.toString();
+    if (url.match('#')) {
+        $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
+    }
+
+    // Change hash for page-reload
+    $('.nav-tabs a').on('shown.bs.tab', function (e) {
+        window.location.hash = e.target.hash;
+        setTimeout(function () {
+            window.scrollTo(0, 0);
+        }, 1);
+    })
+});
